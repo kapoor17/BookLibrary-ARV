a9162ecd833f38d79930de83a3f6f259923e486c
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2022-01-20 15:14:53 +0530

    after this changing vh to %

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Book Library application similar to AMazon Kindle
 
 AR Ventura
+
+design tweaks for future edit
+1: content accordion shouldnt have a height and neither should the book cover..adjust the height through js
--- app.js
+++ app.js
@@ -1,6 +1,30 @@
-const bookCover=document.querySelector(".book-cover");
+const accordionCover=document.querySelector("#accordion-cover");
+const bookCover=accordionCover.querySelector(".book-cover");
 const bookPreview=bookCover.querySelector(".preview-button");
+const bookContent = accordionCover.querySelectorAll(".accordion-item.with-preview");
+
+const mainBook=document.querySelector("#main-book");
+const returnButton=mainBook.querySelector(".return");
+const bookChapters = mainBook.querySelectorAll(".chapters.with-preview");
+
+
+console.log(bookContent);
+console.log(bookChapters);
 
 bookPreview.addEventListener("click",()=>{
     bookCover.classList.toggle("variable-height-class");
+})
+
+bookContent.forEach((content,index)=>{
+    content.addEventListener("click",()=>{
+        // window.location=index.href;
+        mainBook.classList.remove("slide-main-book-out");
+        mainBook.classList.add("slide-main-book-in");
+    })
+    
+})
+
+returnButton.addEventListener("click",()=>{
+    mainBook.classList.add("slide-main-book-out");
+    mainBook.classList.remove("slide-main-book-in");
 })
