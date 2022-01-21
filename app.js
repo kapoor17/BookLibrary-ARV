39040a3479986ae07e2e6613ebaff43421e7291d
Himanshu Kapoor <himanshu.kapur17@gmail.com>
2022-01-21 16:29:49 +0530

    design process save-1

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
-const accordionCover=document.querySelector("#accordion-cover");
-const bookCover=accordionCover.querySelector(".book-cover");
-const bookPreview=bookCover.querySelector(".preview-button");
-const bookContentWP = accordionCover.querySelectorAll(".accordion-item.with-preview");
-const bookContent = accordionCover.querySelectorAll(".accordion-item:not(.with-sub-chapters)");
-
-const mainBook=document.querySelector("#main-book");
-const returnButton=mainBook.querySelector(".return");
-const bookChaptersWP = mainBook.querySelectorAll(".chapters.with-preview");
-const bookChapters = mainBook.querySelectorAll(".chapters:not(.with-sub-chapters)");
-
-
-console.log(bookContent);
-// console.log(bookChapters);
-
-bookPreview.addEventListener("click",()=>{
-    bookCover.classList.toggle("variable-height-class");
-})
-
-bookContent.forEach((content,index)=>{
-    content.addEventListener("click",()=>{
-        if(content.classList.contains("with-preview")){
-            mainBook.classList.remove("slide-main-book-out");
-            mainBook.classList.add("slide-main-book-in");
-            var goToPage=setTimeout(function(){
-                            location.href="#";
-                            location.href="#"+index;
-                            return;
-                        }, 500);
-        }
+    const accordionCover=document.querySelector("#accordion-cover");
+    const bookCover=accordionCover.querySelector(".book-cover");
+    const bookPreview=bookCover.querySelector(".preview-button");
+    const contentlibrary=accordionCover.querySelector(".content-library");
+    const goToTop=accordionCover.querySelector(".go-back");
+    const bookContentWP = accordionCover.querySelectorAll(".accordion-item.with-preview");
+    const bookContent = accordionCover.querySelectorAll(".accordion-item:not(.with-sub-chapters)");
+    
+    const mainBook=document.querySelector("#main-book");
+    const returnButton=mainBook.querySelector(".return");
+    const bookChaptersWP = mainBook.querySelectorAll(".chapters.with-preview");
+    const bookChapters = mainBook.querySelectorAll(".chapters:not(.with-sub-chapters)");
+    
+    
+    console.log(bookContent);
+    // console.log(bookChapters);
+    
+    bookPreview.addEventListener("click",()=>{
+        bookCover.classList.add("variable-height-class");
+        setTimeout(() => {
+            goToTop.classList.add("fixed");
+            contentlibrary.classList.add("pt-5");
+        }, 1000);
     })
-})
 
+    goToTop.addEventListener("click",()=>{
+        goToTop.classList.remove("fixed");
+        contentlibrary.classList.remove("pt-5");
+        bookCover.classList.remove("variable-height-class");
 
-returnButton.addEventListener("click",()=>{
-    mainBook.classList.add("slide-main-book-out");
-    mainBook.classList.remove("slide-main-book-in");
-})
+    })
+    
+    bookContent.forEach((content,index)=>{
+        content.addEventListener("click",()=>{
+            if(content.classList.contains("with-preview")){
+                mainBook.classList.remove("slide-main-book-out");
+                mainBook.classList.add("slide-main-book-in");
+                var goToPage=setTimeout(function(){
+                                location.href="#";
+                                location.href="#"+index;
+                                return;
+                            }, 500);
+            }
+        })
+    })
+    
+    
+    returnButton.addEventListener("click",()=>{
+        mainBook.classList.add("slide-main-book-out");
+        mainBook.classList.remove("slide-main-book-in");
+    }) 
 
